2edc17473c3383e575b9b63c4e27caff5eba8d57
Manuel Henke <manuel.henke@yahoo.de>
2020-02-03 14:15:15 +0100

    add analytics

--- src/main.js
+++ src/main.js
@@ -1,20 +1,24 @@
-import Amplify, * as AmplifyModules from 'aws-amplify'
+import Amplify, * as AmplifyModules from 'aws-amplify';
 import { Logger } from 'aws-amplify';
-import { AmplifyPlugin } from 'aws-amplify-vue'
-import awsconfig from './aws-exports'
-import Vue from 'vue'
-import App from './App.vue'
+import { AmplifyPlugin } from 'aws-amplify-vue';
+import awsconfig from './aws-exports';
+import Vue from 'vue';
+import App from './App.vue';
+import VueAnalytics from 'vue-analytics';
 
-Amplify.configure(awsconfig)
+Amplify.configure(awsconfig);
 
-Vue.use(AmplifyPlugin, AmplifyModules)
+Vue.use(AmplifyPlugin, AmplifyModules);
+Vue.use(VueAnalytics, {
+  id: 'UA-140895510-1 '
+});
 
 // It's important that you instantiate the Vue instance after calling Vue.use!
 
 const logger = new Logger("vue-logger");
 
-logger.info("App initialized")
+logger.info("App initialized");
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App);
+}).$mount('#app');
