6b2245fe122e617c0c9d803fa7b36de68abb9c62
Manuel Henke <manuel.henke@yahoo.de>
2020-02-03 13:37:26 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,85 @@
 <template>
-    <div class="app">
-        <div class="app-header">
-            <h1>Welcome to Henkebyte</h1>
-        </div>
+  <div class="app">
+    <div class="app-header">
+      <h1 class="app-header-title">Henkebyte</h1>
+      <p class="app-header-subtitle">
+        <span class="message">Comming soon&hellip;</span>
+        <span class="contact">
+          <a href="mailto:manuel.henke@yahoo.d">Contact</a>
+        </span>
+      </p>
     </div>
+  </div>
 </template>
 
 <script>
-import { components } from 'aws-amplify-vue'
+import { components } from "aws-amplify-vue";
 
 export default {
-  name: 'app', 
+  name: "app",
   components: {
     ...components
   }
-}
+};
 </script>
 
-<style scoped>
-  .app {
-    background: rgb(2,0,36);
-    background: linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  min-width: auto;
+  height: 100%;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
+
+<style lang="scss" scoped>
+.app {
+  background: rgb(170, 233, 4);
+  background: linear-gradient(
+    180deg,
+    rgba(170, 233, 4, 1) 0%,
+    rgba(0, 212, 255, 1) 100%
+  );
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .app-header {
+    font-family: "Courier New";
+    .app-header-title {
+      margin: 0;
+      text-align: center;
+
+      font-size: 72px;
+      font-weight: 500;
+      color: #000;
+    }
+    .app-header-subtitle {
+      margin: 0;
+
+      display: flex;
+      justify-content: space-between;
+
+      font-weight: 100;
+
+      .message {
+        color: #666666;
+      }
+
+      .contact {
+
+        a {
+          text-decoration: none;
+        }
+      }
+    }
   }
-  .app-header{
-    text-align: center;
-    color: white;
-    font-family: "Courier New"
-  }
+}
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -12,35 +12,45 @@ module.exports = {
     },
     module: {
         rules: [{
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            // this will apply to both plain `.js` files
-            // AND `<script>` blocks in `.vue` files
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            // this will apply to both plain `.css` files
-            // AND `<style>` blocks in `.vue` files
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(png|svg|jpg|gif|ico)$/,
-                use: [
-                    'file-loader',
-                ],
-                exclude: [
-                    /node_modules/,
-                    /static/
-                ]
-            },
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        },
+        // this will apply to both plain `.js` files
+        // AND `<script>` blocks in `.vue` files
+        {
+            test: /\.js$/,
+            loader: 'babel-loader'
+        },
+        // this will apply to both plain `.css` files
+        // AND `<style>` blocks in `.vue` files
+        {
+            test: /\.css$/,
+            use: [
+                'vue-style-loader',
+                'css-loader'
+            ],
+            exclude: /node_modules/
+        },
+        // this will apply to both plain `.scss` files
+        // AND `<style lang="scss">` blocks in `.vue` files
+        {
+            test: /\.scss$/,
+            use: [
+                'vue-style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        },
+        {
+            test: /\.(png|svg|jpg|gif|ico)$/,
+            use: [
+                'file-loader',
+            ],
+            exclude: [
+                /node_modules/,
+                /static/
+            ]
+        },
         ]
     },
     devServer: {
