f35f4578c86dabfbecf36298612e0936ce754ebd
Manuel Henke <manuel.henke@yahoo.de>
2020-02-04 17:47:15 +0100

    update welcome card

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
-  <div class="app">
-    <div class="app-header">
-      <!-- <h1 class="app-header-title">Henkebyte</h1> -->
-      <img src="henkebyte-logo.png" alt="henkebyte-logo" />
-      <div class="line"></div>
-      <div class="app-header-subtitle">
-        <span class="message">Comming soon&hellip;</span>
-      </div>
+  <div class="app container">
+    <WelcomeCard/>
+    <div class="contact-button-container">
+      <ContactButton/>
     </div>
-    <span class="app-contact">
-      <b-button variant="link" class="text-decoration-none" href="mailto:contact@henkebyte.com">
-        <b-icon icon="envelope" scale="1.5"></b-icon>
-        <span class="button-text">Contact</span>
-      </b-button>
-    </span>
   </div>
 </template>
 
 <script>
 import { components } from "aws-amplify-vue";
+import WelcomeCard from "./components/WelcomeCard.vue";
+import ContactButton from "./components/ContactButton.vue";
 
 export default {
   name: "app",
   components: {
-    ...components
+    ...components,
+    WelcomeCard,
+    ContactButton
   },
   mounted() {
     this.$ga.page("/");
@@ -38,59 +32,10 @@ export default {
   align-items: center;
   justify-content: center;
 
-  .app-header {
-    padding: 30px 60px;
-    background-color: rgba(240, 240, 240, 0.5);
-    -webkit-box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.25);
-    -moz-box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.25);
-    box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.25);
-
-    color: #139187;
-    .app-header-title {
-      margin: 0;
-
-      line-height: 100%;
-      font-size: 72px;
-      font-weight: 500;
-    }
-
-    .line {
-      margin: 30px -30px;
-      border-top: #139187 solid 2px;
-    }
-
-    .app-header-subtitle {
-      display: flex;
-      justify-content: center;
-
-      .message {
-        font-weight: 100;
-        font-size: 22px;
-      }
-    }
-  }
-
-  .app-contact {
+  .contact-button-container {
     position: fixed;
     bottom: 15px;
     right: 15px;
-
-    .btn {
-      &:hover {
-        .button-text {
-          margin-left: 5px;
-          width: 60px;
-          opacity: 1;
-        }
-      }
-
-      .button-text {
-        display: inline-block;
-        opacity: 0;
-        width: 0;
-        transition: all 0.5s;
-      }
-    }
   }
 }
 </style>
---
+++ src/components/ContactButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <span class="app-contact">
+    <b-button variant="link" class="text-decoration-none" :href="'mailto:' + this.mail">
+      <b-icon icon="envelope" scale="1.5"></b-icon>
+      <span class="button-text">Contact</span>
+    </b-button>
+  </span>
+</template>
+
+
+<script>
+export default {
+  name: "ContactButton",
+  data() {
+    return {
+      mail: "contact@henkebyte.com"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.app-contact {
+    .btn {
+        &:hover {
+            .button-text {
+                margin-left: 5px;
+                width: 60px;
+                opacity: 1;
+            }
+        }
+
+        .button-text {
+            display: inline-block;
+            opacity: 0;
+            width: 0;
+            transition: all 0.5s;
+        }
+    }
+}
+</style>
