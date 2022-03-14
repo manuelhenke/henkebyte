493f0534f3a95d463c6359459a463483c1bf2254
Manuel Henke <manuel.henke@yahoo.de>
2022-03-14 08:16:08 +0100

    add bootstrap as plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     {
-      src: '~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+      src: '@/plugins/bootstrap.js',
       mode: 'client',
     },
   ],
