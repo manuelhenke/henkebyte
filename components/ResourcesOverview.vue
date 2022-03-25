ea42ddf0cb10ac0eb9ec2fbe0afce6da57a2acdd
Manuel Henke <manuel.henke@yahoo.de>
2022-03-25 14:48:33 +0100

    update resources overview

--- components/ResourcesOverview.vue
+++ components/ResourcesOverview.vue
@@ -1,13 +1,30 @@
 <template comments>
   <div class="resources-overview">
     <div class="search-bar mb-3">
-      <input
-        v-model="currentSearchInput"
-        class="form-control"
-        type="text"
-        placeholder="Search..."
-        aria-label="search resources"
-      />
+      <div class="input-container">
+        <input
+          v-model="currentSearchInput"
+          class="form-control"
+          type="text"
+          placeholder="Search..."
+          aria-label="search resources"
+        />
+        <div
+          v-if="currentSearchInput"
+          class="btn btn-icon clear-input"
+          @click="currentSearchInput = ''"
+        >
+          <i class="bi bi-x"></i>
+        </div>
+      </div>
+      <button
+        class="btn btn-primary d-none d-sm-inline-block"
+        type="button"
+        @click="toggleMasonry"
+      >
+        <i v-if="masonry" class="bi bi-grid"></i>
+        <i v-else class="bi bi-columns-gap"></i>
+      </button>
       <button
         class="btn btn-primary w-100"
         type="button"
@@ -18,7 +35,7 @@
         Filter<i class="bi bi-funnel ms-2"></i>
       </button>
     </div>
-    <div class="d-flex justify-content-center">
+    <div class="error-box">
       <div
         v-if="$fetchState.pending"
         class="spinner-border text-primary"
@@ -36,7 +53,6 @@
       ref="cardContainer"
       class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-3"
     >
-      <!-- card container -->
       <div v-for="site in filteredSites" :key="site.fields.title" class="col">
         <article class="card">
           <img
@@ -47,7 +63,7 @@
               site.fields.title ? site.fields.title : 'the site'
             }`"
             onerror="this.style.display='none'"
-            @error="masonry.layout()"
+            @error="layoutMasonry"
           />
           <div class="card-body">
             <h2 v-if="site.fields.title" class="card-title">
@@ -76,7 +92,6 @@
             ></a>
           </div>
         </article>
-        <!-- /card container -->
       </div>
     </div>
 
@@ -149,6 +164,7 @@
 <script>
 import Masonry from 'masonry-layout'
 import { client } from '~/plugins/contentful.js'
+import { arrayEquals } from '~/util'
 
 export default {
   name: 'ResourcesOverview',
@@ -174,7 +190,9 @@ export default {
         .then(([sites, categories]) => {
           this.sites = sites.items
           this.categories = categories.items
-          this.checkedCategories = categories.items.map((item) => item.sys.id)
+          if (this.checkedCategories.length === 0) {
+            this.checkedCategories = categories.items.map((item) => item.sys.id)
+          }
         })
         // eslint-disable-next-line no-console
         .catch(console.error)
@@ -264,18 +282,70 @@ export default {
   },
   watch: {
     filteredSites() {
-      this.$nextTick(() => {
-        this.masonry.reloadItems()
-        this.masonry.layout()
-      })
+      if (this.masonry) {
+        this.$nextTick(() => {
+          this.masonry.reloadItems()
+          this.layoutMasonry()
+        })
+      }
+    },
+    currentSearchInput() {
+      const query = {
+        ...this.$route.query,
+      }
+      if (this.currentSearchInput) {
+        query.q = this.currentSearchInput
+      } else {
+        delete query.q
+      }
+
+      // Avoid redundant navigation
+      if (this.$route.query.q !== query.q) {
+        this.$router.replace({
+          path: this.$route.path,
+          query,
+        })
+      }
+    },
+    checkedCategories() {
+      // Categories are still not fetched
+      if (this.categories.length === 0) return
+
+      const query = {
+        ...this.$route.query,
+      }
+      if (
+        this.checkedCategories.length > 0 &&
+        this.checkedCategories.length < this.categories.length
+      ) {
+        query.categories = this.checkedCategories
+      } else {
+        delete query.categories
+      }
+
+      // Avoid redundant navigation
+      if (!arrayEquals(this.$route.query.categories, query.categories)) {
+        this.$router.replace({
+          path: this.$route.path,
+          query,
+        })
+      }
     },
   },
   mounted() {
-    this.masonry = new Masonry(this.$refs.cardContainer, {
-      // options
-      itemSelector: '.col',
-      percentPosition: true,
-    })
+    if (!localStorage.getItem('preferGrid')) {
+      this.enableMasonry()
+    }
+    if ('q' in this.$route.query) {
+      this.currentSearchInput = this.$route.query.q
+    }
+    if ('categories' in this.$route.query) {
+      if (Array.isArray(this.$route.query.categories)) {
+        this.checkedCategories = this.$route.query.categories
+      } else {
+        this.checkedCategories = [this.$route.query.categories]
+      }
+    }
   },
   methods: {
     selectAllCategories() {
@@ -283,6 +353,37 @@ export default {
     },
     deselectAllCategories() {
       this.checkedCategories = []
+    },
+    toggleMasonry() {
+      if (this.masonry) {
+        this.disableMasonry()
+      } else {
+        this.enableMasonry()
+      }
+    },
+    enableMasonry() {
+      this.masonry = new Masonry(this.$refs.cardContainer, {
+        // options
+        itemSelector: '.col',
+        percentPosition: true,
+      })
+      if (process.client) {
+        localStorage.removeItem('preferGrid')
+      }
+    },
+    disableMasonry() {
+      if (this.masonry) {
+        this.masonry.destroy()
+        this.masonry = undefined
+      }
+      if (process.client) {
+        localStorage.setItem('preferGrid', true)
+      }
+    },
+    layoutMasonry() {
+      if (this.masonry) {
+        this.masonry.layout()
+      }
     },
   },
 }
@@ -300,14 +401,40 @@ export default {
     flex-direction: row;
   }
 
-  > input {
+  .input-container {
     flex: 1 0;
+    position: relative;
+
+    .clear-input {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      display: inline-flex;
+      place-content: center;
+      place-items: center;
+      padding-inline: map-get($spacers, 3);
+      cursor: pointer;
+    }
   }
 
-  > button {
+  *:not(.input-container) {
     flex: 0 1;
     white-space: nowrap;
   }
+}
+
+.error-box {
+  display: flex;
+  place-content: center;
+  place-items: center;
+  &:not(:empty) {
+    margin-top: map-get($spacers, 5);
+  }
+}
+
+#card-container:empty {
+  display: none;
 }
 
 .card {
--- util/index.js
+++ util/index.js
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param {number} timestamp
+ * @returns {string}
+ */
 export function timestampToDateString(timestamp) {
   const event = new Date(timestamp)
   const options = {
@@ -9,6 +14,27 @@ export function timestampToDateString(timestamp) {
   return event.toLocaleDateString('en-EN', options)
 }
 
+/**
+ *
+ * @param {string} string
+ * @returns {boolean}
+ */
 export function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+/**
+ *
+ * @param {*[]} a
+ * @param {*[]} b
+ * @returns {boolean}
+ */
+export function arrayEquals(a, b) {
+  if (a === b) return true
+  return (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((v, i) => v === b[i])
+  )
 }
