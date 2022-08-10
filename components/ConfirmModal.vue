f9ca41643c54ba50a89edb5246bca7f75b7ff645
Manuel Henke <manuel.henke@yahoo.de>
2022-08-10 15:47:32 +0200

    refactor: create dedicated component

---
+++ components/ConfirmModal.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="modal fade" tabindex="-1" :aria-labelledby="titleId" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 :id="titleId" class="modal-title">{{ title }}</h5>
+          <button
+            v-if="!noCloseButton"
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          {{ body }}
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            :class="`btn ${cancelButtonType}`"
+            data-bs-dismiss="modal"
+            @click="$emit('cancel')"
+          >
+            {{ cancelButtonText }}
+          </button>
+          <button
+            type="button"
+            :class="`btn ${confirmButtonType}`"
+            data-bs-dismiss="modal"
+            @click="$emit('confirm')"
+          >
+            {{ confirmButtonText }}
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { uniqueId } from 'lodash-es';
+
+export default {
+  name: 'ConfirmModal',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    cancelButtonText: {
+      type: String,
+      required: false,
+      default: 'Cancel',
+    },
+    cancelButtonType: {
+      type: String,
+      required: false,
+      default: 'btn-outline-dark',
+    },
+    confirmButtonText: {
+      type: String,
+      required: false,
+      default: 'Confirm',
+    },
+    confirmButtonType: {
+      type: String,
+      required: false,
+      default: 'btn-primary',
+    },
+    noCloseButton: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  computed: {
+    titleId() {
+      return uniqueId('modal-');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- components/MinesweeperWrapper.vue
+++ components/MinesweeperWrapper.vue
@@ -155,82 +155,22 @@
       </div>
     </section>
 
-    <div
+    <ConfirmModal
       id="restart-modal"
-      class="modal fade"
-      tabindex="-1"
-      aria-labelledby="restartModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 id="restartModalLabel" class="modal-title">Restart the game</h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            Are you sure, that you want to restart the game? Any Progress will be lost.
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">
-              Cancel
-            </button>
-            <button
-              type="button"
-              class="btn btn-primary"
-              data-bs-dismiss="modal"
-              @click="restartGame"
-            >
-              Yes, restart
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+      title="Restart the game"
+      body="Are you sure, that you want to restart the game? Any Progress will be lost."
+      confirm-button-text="Yes, restart"
+      @confirm="restartGame"
+    />
 
-    <div
+    <ConfirmModal
       id="reset-game-history-modal"
-      class="modal fade"
-      tabindex="-1"
-      aria-labelledby="resetGameHistoryModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 id="resetGameHistoryModalLabel" class="modal-title">Reset game history</h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            Are you sure, that you want to reset the entire history (including every game mode)?
-            This action is irreversible.
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">
-              Cancel
-            </button>
-            <button
-              type="button"
-              class="btn btn-danger"
-              data-bs-dismiss="modal"
-              @click="resetGameHistory"
-            >
-              Yes, reset
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+      title="Reset game history"
+      body="Are you sure, that you want to reset the entire history (including every game mode)? This action is irreversible."
+      confirm-button-text="Yes, reset"
+      confirm-button-type="btn-danger"
+      @confirm="resetGameHistory"
+    />
 
     <RainOverlay ref="rain" no-init-rain />
     <div ref="firework" class="firework-container"></div>
