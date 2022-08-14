3508092ba7080b1bf8951409d61946ddba056399
Manuel Henke <manuel.henke@yahoo.de>
2022-08-14 12:46:58 +0200

    chore: use nuxt-envalid module

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
-import { str } from 'envalid';
+/* eslint-disable import/no-extraneous-dependencies */
+import { str } from 'nuxt-envalid';
 import { getRoutes, mapRoutes } from './util/routesUtils';
 
 const buildDescription = (target) =>
@@ -120,7 +121,8 @@ export default {
     '@nuxtjs/stylelint-module',
     // https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
-    '~/modules/nuxt-envalid.js',
+    // https://github.com/manuelhenke/nuxt-envalid
+    'nuxt-envalid',
     // https://sitemap.nuxtjs.org/
     '@nuxtjs/sitemap',
     // https://github.com/nuxt-community/robots-module
@@ -216,7 +218,7 @@ export default {
   },
 
   envalid: {
-    validators: {
+    specs: {
       CTF_SPACE_ID: str(),
       CTF_CDA_ACCESS_TOKEN: str(),
       CTF_ENVIRONMENT: str({ default: '' }),
