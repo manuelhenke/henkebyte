42e98eb0f25b644292d27ff57b0f424dd3e932d9
Manuel Henke <manuel.henke@yahoo.de>
2022-10-11 14:52:17 +0200

    fix(minesweeper): calculate animation duration

--- nuxt.config.js
+++ nuxt.config.js
@@ -208,7 +208,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: ['@nuxtjs/color-mode'],
+    transpile: ['@nuxtjs/color-mode', 'fireworks-js'],
     postcss: {
       plugins: [],
     },
