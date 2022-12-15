03c44786abbdea72b5e4073177f983e7a2108d8f
Manuel Henke <manuel.henke@yahoo.de>
2022-12-15 14:21:44 +0100

    chore: transpile chart.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -208,7 +208,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: ['@nuxtjs/color-mode', 'fireworks-js'],
+    transpile: ['@nuxtjs/color-mode', 'fireworks-js', 'chart.js'],
     postcss: {
       plugins: [],
     },
