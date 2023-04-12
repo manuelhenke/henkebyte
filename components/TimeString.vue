40ab465ff9e83dc1d9e25ff7000719c19791b1fe
Manuel Henke <manuel.henke@yahoo.de>
2023-04-12 15:11:37 +0200

    feat(minesweeper): basic sharing option (#430)

--- components/TimeString.vue
+++ components/TimeString.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+import { millisecondsToTimeString } from '@/util/index.js';
+
 export default {
   name: 'TimeString',
   props: {
@@ -13,11 +15,7 @@ export default {
   },
   computed: {
     formattedElapsedTime() {
-      const overallSeconds = Math.floor(this.milliseconds / 1000);
-      const minutes = Math.floor(overallSeconds / 60);
-      const seconds = overallSeconds % 60;
-
-      return `${minutes}:${String(seconds).padStart(2, '0')}`;
+      return millisecondsToTimeString(this.milliseconds);
     },
     timeStringStyle() {
       // avoid bumping on often number change, e.g. when used together with StopWatch
